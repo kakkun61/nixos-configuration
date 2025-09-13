d72bda53a2b89b7baba067a5278b719fc0330b71
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2025-09-13 17:30:51 +0900

    しごと用 config も Flakes に

--- flake.nix
+++ flake.nix
@@ -33,6 +33,9 @@
             ];
           };
 
+          time.timeZone = "Asia/Tokyo";
+          i18n.defaultLocale = "ja_JP.UTF-8";
+
           programs.nix-ld.enable = true;
 
           users = {
@@ -42,7 +45,9 @@
               group = "wheel";
               # This automatically sets group to users, createHome to true, home to /home/«username», useDefaultShell to true, and isSystemUser to false.
               isNormalUser = true;
-              openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKFQWOqFI7MX3rdZ6hNibGYDnZvBveyVEpeyiVNbWHfn+EWlDHSgOYNc8MCRaxYU+c89v6usZAGe7XDDj4QSEKwaNhFiCf30Jq5iihc9urc0K+fmwOSwDPgI5Hbr6BLqc3xdxhYzj2unzzcna9+1G32GX9SHz9nkSAgUV63wjSOx5hiQ2pDhk9xI/L63OjC0PVNw95wl1AA/+DZdmTpjcM+2V2Wx5ZL9LIM5lU5rXqHZ7RmOdSrjvB3aU+ju0XqfqR6Ixt10D+/8y5bn4/Pyz44PhPHC/dsI/y6Q7IEX8H0eCchmXtM2pH1LRNW1W52NkucmyJGwBrqHJLvdgMpPCU/32IR1OLNnoFodLkATgbSSONOQiGO8i07mwuSd2zQ7PUM1Qg98J3VR8YW8IJVQF4QJoBD0qREyIi4STe3r/Z8HJFBJsF+lSGgwBCUTUKEptMYQLAerk9uUbcmGM6ERhzAFZuPhTl/xEq4yK4+wTsf++87QkgWgaPHF16OUw2+Vjc45OzzfdjOuE09N38zk8GJCjV6NBpHgK9srvepccurC5jlcmUwGmdIq21OItHK6JCw18LG6T0pB5n/yiA+aV8ZJCoJkdS/p5+QMLmKiX15sAmyuSlLPgDAG6flhT2ppamZSbQDsC22fVaBH12dRIR+coaD1BbCKTlJYzyIz/8yw== kazuki@Maihama" ];
+              openssh.authorizedKeys.keys = [
+                "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKFQWOqFI7MX3rdZ6hNibGYDnZvBveyVEpeyiVNbWHfn+EWlDHSgOYNc8MCRaxYU+c89v6usZAGe7XDDj4QSEKwaNhFiCf30Jq5iihc9urc0K+fmwOSwDPgI5Hbr6BLqc3xdxhYzj2unzzcna9+1G32GX9SHz9nkSAgUV63wjSOx5hiQ2pDhk9xI/L63OjC0PVNw95wl1AA/+DZdmTpjcM+2V2Wx5ZL9LIM5lU5rXqHZ7RmOdSrjvB3aU+ju0XqfqR6Ixt10D+/8y5bn4/Pyz44PhPHC/dsI/y6Q7IEX8H0eCchmXtM2pH1LRNW1W52NkucmyJGwBrqHJLvdgMpPCU/32IR1OLNnoFodLkATgbSSONOQiGO8i07mwuSd2zQ7PUM1Qg98J3VR8YW8IJVQF4QJoBD0qREyIi4STe3r/Z8HJFBJsF+lSGgwBCUTUKEptMYQLAerk9uUbcmGM6ERhzAFZuPhTl/xEq4yK4+wTsf++87QkgWgaPHF16OUw2+Vjc45OzzfdjOuE09N38zk8GJCjV6NBpHgK9srvepccurC5jlcmUwGmdIq21OItHK6JCw18LG6T0pB5n/yiA+aV8ZJCoJkdS/p5+QMLmKiX15sAmyuSlLPgDAG6flhT2ppamZSbQDsC22fVaBH12dRIR+coaD1BbCKTlJYzyIz/8yw== kazuki@Maihama"
+              ];
               packages = with pkgs; [ home-manager ];
               useDefaultShell = true;
             };
@@ -84,6 +89,43 @@
                 }
               )
               common-config
+            ];
+          };
+          work = nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./hardware-configuration.nix
+              { system.stateVersion = "24.05"; }
+              (
+                { pkgs, ... }:
+                {
+                  boot.loader.grub.enable = true;
+                  boot.loader.grub.device = "/dev/sdb";
+                  # /dev/sda は swap 用
+
+                  networking.hostName = "herp";
+
+                  services = {
+                    avahi = {
+                      enable = true;
+                      nssmdns4 = true;
+                      publish = {
+                        enable = true;
+                        addresses = true;
+                        domain = true;
+                      };
+                    };
+                    openssh = {
+                      enable = true;
+                      extraConfig = ''
+                        AllowAgentForwarding yes
+                      '';
+                    };
+                  };
+
+                  virtualisation.docker.enable = true;
+                }
+              )
             ];
           };
         };
