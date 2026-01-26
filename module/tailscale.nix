af7506d04b2d4450270f492ee32e082b04fd8e71
Kazuki Okamoto (岡本和樹) <kazuki.okamoto@kakkun61.com>
2026-01-26 14:31:24 +0900

    tailscale は systemd-resolved と協調動作するようになっている

--- module/tailscale.nix
+++ module/tailscale.nix
@@ -1,6 +1,13 @@
 {
-  services.tailscale = {
-    enable = true;
-    authKeyFile = "/home/kazuki/.config/tailscale/auth.key";
+  services = {
+    tailscale = {
+      enable = true;
+      authKeyFile = "/home/kazuki/.config/tailscale/auth.key";
+    };
+    # > Linuxディストリビューションのメンテナーの皆様は、ユーザーにどのような設定を強制すべきかお悩みかもしれません。
+    # > 当社の見解では、systemd-resolvedを使用し、ユーザーフレンドリーなネットワーク設定が必要な場合は、
+    # > 最新バージョンのNetworkManager（1.26.6以降）を採用することをお勧めします。
+    # https://tailscale.com/blog/sisyphean-dns-client-linux
+    resolved.enable = true;
   };
 }
