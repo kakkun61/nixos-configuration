5d70608b3ec1778fbfdeee77fd7c16f82bc2bc4b
Kazuki Okamoto (岡本和樹) <kazuki.okamoto@kakkun61.com>
2026-02-02 01:19:13 +0900

    Kubernetes 向けファイアウォール

--- module/kubernetes.nix
+++ module/kubernetes.nix
@@ -1,10 +1,25 @@
 { secretPath, ... }:
 {
   imports = [ ./secret.nix ];
+
   config = {
     services.k3s = {
       enable = true;
       tokenFile = "${secretPath}/k3s";
+    };
+
+    # K3s のドキュメントの指示
+    # https://docs.k3s.io/ja/installation/requirements#オペレーティングシステム
+    networking.firewall = {
+      allowedTCPPorts = [ 6443 ];
+      extraCommands = ''
+        iptables -A INPUT -s 10.42.0.0/16 -j ACCEPT
+        iptables -A INPUT -s 10.43.0.0/16 -j ACCEPT
+      '';
+      extraStopCommands = ''
+        iptables -D INPUT -s 10.42.0.0/16 -j ACCEPT
+        iptables -D INPUT -s 10.43.0.0/16 -j ACCEPT
+      '';
     };
   };
 }
